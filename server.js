47ce27b8ec32d54b83288c0eabd39c248fe4d74e
Hector P <pizano_hector@outlook.com>
2023-09-22 12:57:39 -0500

    GET & PUT SQL statement added

--- server.js
+++ server.js
@@ -3,20 +3,52 @@ const client = new pg.Client('postgres://localhost/movie_ratings_db');
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.json())
 
-const homePage = path.join(__dirname, 'index.html')
+const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res) => res.sendFile(homePage));
 
-const reactApp= path.join(__dirname, 'dist/main.js')
+const reactApp= path.join(__dirname, 'dist/main.js');
 app.get('/dist/main.js', (req, res) => res.sendFile(reactApp));
 
-const reactSourceMap= path.join(__dirname, 'dist/main.js.map')
+const reactSourceMap= path.join(__dirname, 'dist/main.js.map');
 app.get('/dist/main.js.map', (req, res) => res.sendFile(reactSourceMap));
 
 
-const styleSheet= path.join(__dirname, 'style.css')
+const styleSheet= path.join(__dirname, 'style.css');
 app.get('/style.css', (req, res) => res.sendFile(styleSheet));
 
+//START OF EXPRESS ROUTES
+
+//GET route /api/movies
+app.get('/api/movies', async(req, res, next) => {
+    try {
+        const SQL =`
+        SELECT * FROM movies
+        ORDER by id
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch(error){
+        next(error);
+    };
+});
+
+//PUT /api/movies/:id
+app.put('/api/movies/:id', async(req, res, next) => {
+    try {
+        const SQL =`
+        UPDATE movies
+        SET name = $1, stars = $2
+        WHERE id = $3
+        RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.name, req.body.stars, req.params.id]);
+        res.send(response.rows[0]);
+    } catch(error){
+        next(error);
+    };
+});
 
 const start = async() => {
     await client.connect();
@@ -28,9 +60,9 @@ const start = async() => {
         name VARCHAR(100),
         stars INTEGER
     );
-    INSERT INTO movies(name, stars) values('Episode IV: A New Hope', 5);
+    INSERT INTO movies(name, stars) values('Episode IV: A New Hope', 2);
     INSERT INTO movies(name, stars) values('Episode V: The Empire Strikes Back', 3);
-    INSERT INTO movies(name, stars) values('Episode VI: Return of the Jedi', 4);
+    INSERT INTO movies(name, stars) values('Episode VI: Return of the Jedi', 1);
     INSERT INTO movies(name, stars) values('Episode I: The Phantom Menace', 4);
     INSERT INTO movies(name, stars) values('Episode II: Attack of the Clones', 5);
     INSERT INTO movies(name, stars) values('Episode III: Revenge of the Sith', 5);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = () => {
-    
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async() => {
+            const response = await axios.get('/api/movies');
+            setMovies(response.data);
+        }
+        fetchMovies();
+    }, []);
+
+    const increaseRating = async(movie) => {
+        const newRating = movie.stars + 1 ;
+        const {data} = await axios.put(`/api/movies/${movie.id}`, {name: movie.name, stars: newRating}) ;
+        const newMovies = movies.map((movieMap) => {
+            if(movieMap.id === movie.id) {
+                return data
+            } else {
+                return movieMap;
+            };
+        });
+        setMovies(newMovies);
+    };
+
+    const decreaseRating = async (movie) => {
+        const newRating = movie.stars - 1 ;
+        const {data} = await axios.put(`/api/movies/${movie.id}`, {name: movie.name, stars: newRating});
+        const newMovies = movies.map((movieMap) => {
+            if(movieMap.id === movie.id) {
+                return data
+            } else {
+                return movieMap;
+            };
+        });
+        setMovies(newMovies);
+    };
+
     return (
-        <h1> Full Stack Template</h1>
+        <div>
+          <h1> Rate the movie! ({movies.length})</h1>
+          <div>
+            <ul>
+                {
+                    movies.map((movie) => {
+                        return(
+                            <li key={movie.id}>                                
+                                <h2>{movie.name}</h2>
+                                <span>
+                                <h3> Rating: {movie.stars} Stars </h3>
+                                <button onClick= { () => {increaseRating(movie)}}> + </button>
+                                <button onClick={() => {decreaseRating(movie)}}> - </button>
+                                </span>
+                                <div>
+                                <button> Delete </button>
+                                </div>
+                            </li>
+                        
+                        )
+                    })
+                }
+            </ul>
+          </div>
+        </div>
     );
 };
 
